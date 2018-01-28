e1fd0020ab66459dd64cea6b10deb426888f0396
Anrin N <8159116+KevinNeon@users.noreply.github.com>
2018-01-28 18:00:18 +0530

    Update eShop.js

--- wavelength-plugins/eShop.js
+++ wavelength-plugins/eShop.js
@@ -23,14 +23,14 @@ function NewItem(name, desc, price, isSSB) {
 
 function writeShop() {
 	if (!writeJSON) return false; //Prevent corruptions
-	fs.writeFile('config/sHop.json', JSON.stringify(WL.eShop), () => {});
+	fs.writeFile('config/eShop.json', JSON.stringify(WL.eShop), () => {});
 }
 
 function shopDisplay() {
-	let output = '<div style="max-height:300px; width: 100%; overflow: scroll"><table style="border:2px solid #101ad1; border-radius: 5px; width: 100%;"><tr><th colspan="3" style="border: 2px solid #070e96; border-radius: 5px">' + Config.serverName + ' Shop</th></tr>';
+	let output = '<div style="max-height:300px; width: 100%; overflow: scroll"><table style="border:2px solid #101ad1; border-radius: 5px; width: 100%;"><tr><th colspan="3" style="border: 2px solid #070e96; border-radius: 5px">Server Shop</th></tr>';
 	for (let i in WL.eShop) {
 		if (!WL.eShop[i]) continue;
-		output += '<tr><td style="border: 2px solid #070e96; width: 20%; text-align: center"><button name="send" value="/shop buy ' + WL.eShop[i].id + '">' + WL.eShop[i].name + '</button></td><td style="border: 2px solid #070e96; width: 70%; text-align: center">' + WL.eShop[i].desc + '</td><td style="border: 2px solid #070e96; width: 10%; text-align: center">' + WL.eShop[i].price + '</td></tr>';
+		output += '<tr><td style="border: 2px solid #070e96; width: 20%; text-align: center"><button name="send" value="/eshop buy ' + WL.eShop[i].id + '">' + WL.eShop[i].name + '</button></td><td style="border: 2px solid #070e96; width: 70%; text-align: center">' + WL.eShop[i].desc + '</td><td style="border: 2px solid #070e96; width: 10%; text-align: center">' + WL.eShop[i].price + '</td></tr>';
 	}
 	output += '</table></div>';
 	return output;
@@ -71,17 +71,27 @@ function toToken(item) {
 	case 'disableintroscroll':
 		return 'disableintroscroll';
 
+	case 'profilemusic':
+	case 'music':
+		return 'music';
+
+	case 'profilebackground':
+	case 'profilebg':
+	case 'background':
+	case 'bg':
+		return 'bg';
+
 	default:
 		return false;
 	}
 }
 
 try {
-	fs.accessSync('config/sHop.json', fs.F_OK);
-	let raw = JSON.parse(fs.readFileSync('config/sHop.json', 'utf8'));
+	fs.accessSync('config/eShop.json', fs.F_OK);
+	let raw = JSON.parse(fs.readFileSync('config/eShop.json', 'utf8'));
 	WL.eShop = raw;
 } catch (e) {
-	fs.writeFile('config/sHop.json', "{}", function (err) {
+	fs.writeFile('config/eShop.json', "{}", function (err) {
 		if (err) {
 			console.error('Error while loading eShop: ' + err);
 			WL.eShop = {
@@ -89,7 +99,7 @@ try {
 			};
 			writeJSON = false;
 		} else {
-			console.log("config/sHop.json not found, creating a new one...");
+			console.log("config/eShop.json not found, creating a new one...");
 		}
 	});
 }
@@ -106,34 +116,36 @@ try {
 
 exports.commands = {
 	//shop: 'eshop', //Uncomment this if you want this to be able to be used using the /shop command
-	shop: {
+	eshop: {
 		add: function (target, room, user, connection, cmd, message) {
 			if (!this.can('roomowner')) return false;
 			if (!allowThisShop) return this.errorReply('This shop is closed');
 			if (WL.eShop.closed) return this.sendReply('An error closed the shop.');
 			target = target.split(',');
-			if (!target[2]) return this.parse('/shop help');
+			if (!target[2]) return this.parse('/eshop help');
 			if (WL.eShop[toId(target[0])]) return this.errorReply(target[0] + ' is already in the shop.');
-			if (isNaN(Number(target[2]))) return this.parse('/shop help');
+			if (isNaN(Number(target[2]))) return this.parse('/eshop help');
 			let isSSB = false;
 			if (toId(target[0]) === 'shiny' || toId(target[0]) === 'ffacustomsymbol' || toId(target[0]) === 'customability' || toId(target[0]) === 'customitem' || toId(target[0]) === 'custommove') isSSB = true;
 			WL.eShop[toId(target[0])] = new NewItem(target[0], target[1], target[2], isSSB);
 			writeShop();
 			return this.sendReply('The item ' + target[0] + ' was added.');
 		},
+
 		remove: function (target, room, user, connection, cmd, message) {
 			if (!allowThisShop) return this.errorReply('This shop is closed');
 			if (!this.can('roomowner')) return false;
 			if (WL.eShop.closed) return this.sendReply('An error closed the shop.');
-			if (!target) return this.parse('/shop help');
+			if (!target) return this.parse('/eshop help');
 			if (!WL.eShop[toId(target)]) return this.errorReply(target + ' is not in the shop.');
 			delete WL.eShop[toId(target)];
 			writeShop();
 			return this.sendReply('The item ' + target + ' was removed.');
 		},
+
 		buy: function (target, room, user, connection, cmd, message) {
 			if (!allowThisShop) return this.errorReply('This shop is closed');
-			if (!target) return this.parse('/shop help buy');
+			if (!target) return this.parse('/eshop help buy');
 			if (WL.eShop.closed) return this.sendReply('The shop is closed, come back later.');
 			if (!WL.eShop[toId(target)]) return this.errorReply('Item ' + target + ' not found.');
 			let item = WL.eShop[toId(target)];
@@ -203,23 +215,25 @@ exports.commands = {
 								WL.messageSeniorStaff(user.name + ' has purchased a ' + item.name + '.');
 							}
 						}
-						user.sendTo(room, "|uhtmlchange|eshop" + user.userid + "|<div style='max-height:300px'><table style='border:2px solid #101ad1; border-radius: 5px'><tr><th colspan='3' style='border: 2px solid #070e96; border-radius: 5px'>" + Config.serverName + " Shop</th></tr><tr><td style='colspan: 3; border: 2px solid #070e96; border-radius: 5px'><center>You have purchased a " + item.name + ". " + (item.id === 'customsymbol' ? "You may now use /customsymbol [symbol] to change your symbol." : "Upper staff have been notified of your purchase and will contact you shortly.") + "</center></td></tr><tr><td colspan='3' style='text-align:center'><button name='send' value='/shop reopen'>Return to Shop</button></td></tr></table>");
+						user.sendTo(room, "|uhtmlchange|eshop" + user.userid + "|<div style='max-height:300px'><table style='border:2px solid #101ad1; border-radius: 5px'><tr><th colspan='3' style='border: 2px solid #070e96; border-radius: 5px'>Server Shop</th></tr><tr><td style='colspan: 3; border: 2px solid #070e96; border-radius: 5px'><center>You have purchased a " + item.name + ". " + (item.id === 'customsymbol' ? "You may now use /customsymbol [symbol] to change your symbol." : "Upper staff have been notified of your purchase and will contact you shortly.") + "</center></td></tr><tr><td colspan='3' style='text-align:center'><button name='send' value='/eshop reopen'>Return to Shop</button></td></tr></table>");
 					});
 				});
 			});
 		},
+
 		help: function (target, room, user, connection, cmd, message) {
 			let reply = '<b>Shop commands</b><br/>';
-			reply += '/shop - Load the shop screen.<br/>';
-			reply += '/shop buy [item] - Buy an item from the shop.<br/>';
+			reply += '/eshop - Load the shop screen.<br/>';
+			reply += '/eshop buy [item] - Buy an item from the shop.<br/>';
 			if (user.can('roomowner')) {
 				reply += '<b>Administrative shop commands:</b><br/>';
-				reply += '/shop add [item name], [description], [price], (is a SSBFFA item) - Adds a item to the shop.<br/>';
+				reply += '/eshop add [item name], [description], [price], (is a SSBFFA item) - Adds a item to the shop.<br/>';
 				reply += 'Valid SSBFFA items are: shiny, ffacustommove, customitem, customability, custommove.<br/>';
-				reply += '/shop remove [item] - removes a item from the shop.<br/>';
+				reply += '/eshop remove [item] - removes a item from the shop.<br/>';
 			}
 			return this.sendReplyBox(reply);
 		},
+
 		reopen: '',
 		'': function (target, room, user, connection, cmd, message) {
 			if (!allowThisShop) return this.errorReply('This shop is closed');
